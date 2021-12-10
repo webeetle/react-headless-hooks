e315b84100b43fc8495c8a6d1ecfabbc5d0e2d99
Riccardo Tartaglia <tartaglia.riccardo@gmail.com>
2021-12-10 16:31:24 +0100

    ehn

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,14 @@ import useDropdown from './useDropdown';
 import useStepper from './useStepper';
 import useTooltip from './useTooltip';
 import useSwitch from './useTooltip';
+import useProgressBar from './useProgressBar';
 import useHasClickedOutside from './useHasClickedOutside';
 
-export { useDropdown, useHasClickedOutside, useStepper, useSwitch, useTooltip };
+export {
+  useDropdown,
+  useHasClickedOutside,
+  useProgressBar,
+  useStepper,
+  useSwitch,
+  useTooltip,
+};
